6bfad624cd4ce3ec941ff9188dbb289f3bc0e701
Chaeeel <janmichquianzon@gmail.com>
2023-08-10 00:59:24 +0800

    5th commit

--- TechEx/index.js
+++ TechEx/index.js
@@ -1,71 +1,34 @@
-// var jwt = localStorage.getItem("jwt");
-// if (jwt == null) {
-//   window.location.href = "./login.html";
-// }
+var jwt = localStorage.getItem("jwt");
+if (jwt == null) {
+  window.location.href = "login.html";
+}
 
-// function loadUser() {
-//   const xhttp = new XMLHttpRequest();
-//   xhttp.open("GET", "https://netzwelt-devtest.azurewebsites.net/Territories/All");
-//   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//   xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
-//   xhttp.send();
-//   xhttp.onreadystatechange = function () {
-//     if (this.readyState == 4) {
-//       const objects = JSON.parse(this.responseText);
-//       if (objects["status"] == "ok") {
-//         const user = objects["user"];
-//         document.getElementById("fname").innerHTML = user["fname"];
-//         document.getElementById("avatar").src = user["avatar"];
-//         document.getElementById("username").innerHTML = user["username"];
-//       }
-//     }
-//   };
-// }
+window.onload = async () => {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    // Fetch and display hierarchical tree from api
+    const treeResponse = await fetch('https://netzwelt-devtest.azurewebsites.net/Territories/All');
+    const treeData = await treeResponse.json();
+    const territoryTree = document.getElementById('territoryTree');
+    displayTree(treeData, territoryTree);
+    
+};
 
-// loadUser();
+function displayTree(node, parentElement) {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+    li.textContent = node.name;
+    ul.appendChild(li);
 
-// function logout() {
-//   localStorage.removeItem("jwt");
-//   window.location.href = "./login.html";
-// }
+    if (node.children) {
+        for (const child of node.children) {
+            displayTree(child, ul);
+        }
+    }
 
-// import express from "express"; 
-// import bodyParser from "body-parser"; 
-// import fetch from "node-fetch";
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const fetch = require('node-fetch'); // Install the 'node-fetch' package: npm install node-fetch
+    parentElement.appendChild(ul);
+}
 
-// const app = express();
-// const port = 3001; // Choose a port for your proxy server
-
-// app.use(bodyParser.json());
-
-// // CORS middleware to allow requests from your front-end domain
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// });
-
-// // Proxy route to forward requests to the API server
-// app.all('/api/*', async (req, res) => {
-//     const url = `https://netzwelt-devtest.azurewebsites.net${req.originalUrl.replace('/api', '')}`;
-//     try {
-//         const response = await fetch(url, {
-//             method: req.method,
-//             headers: req.headers,
-//             body: JSON.stringify(req.body)
-//         });
-//         const data = await response.json();
-//         res.json(data);
-//     } catch (error) {
-//         console.error('Error in proxy:', error);
-//         res.status(500).json({ error: 'An error occurred in the proxy' });
-//     }
-// });
-
-// app.listen(port, () => {
-//     console.log(`Proxy server is running on port ${port}`);
-// });
+function logout() {
+  localStorage.removeItem("jwt");
+  window.location.href = "login.html";
+}
